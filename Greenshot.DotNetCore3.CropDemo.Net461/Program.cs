810bdb619cee51dfc33536fa324e283f3113bed2
Robin <robin@dapplo.net>
2018-10-19 07:54:58 +0200

    Made 2 projects, with shared code, to have the net461 and dotnet core 3.0 side by side to test

---
+++ Greenshot.DotNetCore3.CropDemo.Net461/Program.cs
@@ -0,0 +1,31 @@
+﻿using Greenshot.Addons.Core;
+using System;
+using System.Windows.Forms;
+using Dapplo.Windows.Desktop;
+using System.Linq;
+using Dapplo.Windows.Dpi;
+
+namespace Greenshot.DotNetCore3.CropDemo.Net461
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // This should not be needed, the app.manifest should do this, but somehow it doesn't
+            NativeDpiMethods.EnableDpiAware();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var screenbounds = WindowCapture.GetScreenBounds();
+            var capture = WindowCapture.CaptureRectangleFromDesktopScreen(new Capture(), screenbounds);
+            var windows = InteropWindowQuery.GetTopLevelWindows().ToList();
+            var form = new CaptureForm(CoreConfiguration.Instance, capture, windows);
+
+            Application.Run(form);
+        }
+    }
+}
--- Greenshot.DotNetCore3.CropDemo/Program.cs
+++ Greenshot.DotNetCore3.CropDemo/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Windows.Forms;
 using Dapplo.Windows.Desktop;
 using System.Linq;
+using Dapplo.Windows.Dpi;
 
 namespace Greenshot.DotNetCore3.CropDemo
 {
@@ -14,10 +15,13 @@ namespace Greenshot.DotNetCore3.CropDemo
         [STAThread]
         static void Main()
         {
+            // This should not be needed, the app.manifest should do this, but somehow it doesn't
+            NativeDpiMethods.EnableDpiAware();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            var capture = WindowCapture.CaptureRectangleFromDesktopScreen(new Capture(), WindowCapture.GetScreenBounds());
+            var screenbounds = WindowCapture.GetScreenBounds();
+            var capture = WindowCapture.CaptureRectangleFromDesktopScreen(new Capture(), screenbounds);
             var windows = InteropWindowQuery.GetTopLevelWindows().ToList();
             var form = new CaptureForm(CoreConfiguration.Instance, capture, windows);
 
