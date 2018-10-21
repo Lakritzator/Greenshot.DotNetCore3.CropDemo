153b070a0d5dd795f90f3865a341e8d05b13ee89
Robin <robin@dapplo.net>
2018-10-21 21:01:53 +0200

    Made the keys work again

---
+++ Greenshot.DotNetCore3.CropDemo.Net461/StartCaptureForm.cs
@@ -0,0 +1,25 @@
+﻿using Dapplo.Windows.Desktop;
+using Greenshot.Addons.Core;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Greenshot.DotNetCore3.CropDemo.Net461
+{
+    public partial class StartCaptureForm : Form
+    {
+        public StartCaptureForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCapture_Click(object sender, EventArgs e)
+        {
+            var screenbounds = WindowCapture.GetScreenBounds();
+            var capture = WindowCapture.CaptureRectangleFromDesktopScreen(new Capture(), screenbounds);
+            var windows = InteropWindowQuery.GetTopLevelWindows().ToList();
+            var form = new CaptureForm(CoreConfiguration.Instance, capture, windows);
+            form.ShowDialog();
+        }
+    }
+}
--- Greenshot.DotNetCore3.CropDemo/Program.cs
+++ Greenshot.DotNetCore3.CropDemo/Program.cs
@@ -4,28 +4,29 @@ using System.Windows.Forms;
 using Dapplo.Windows.Desktop;
 using System.Linq;
 using Dapplo.Windows.Dpi;
+using Dapplo.Log;
+using Dapplo.Log.Loggers;
 
 namespace Greenshot.DotNetCore3.CropDemo
 {
     static class Program
     {
+        private static readonly LogSource Log = new LogSource();
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            LogSettings.RegisterDefaultLogger<DebugLogger>(LogLevels.Verbose);
+
             // This should not be needed, the app.manifest should do this, but somehow it doesn't
             NativeDpiMethods.EnableDpiAware();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            var screenbounds = WindowCapture.GetScreenBounds();
-            var capture = WindowCapture.CaptureRectangleFromDesktopScreen(new Capture(), screenbounds);
-            var windows = InteropWindowQuery.GetTopLevelWindows().ToList();
-            var form = new CaptureForm(CoreConfiguration.Instance, capture, windows);
-
-            Application.Run(form);
+            var startCaptureForm = new StartCaptureForm();
+            Application.Run(startCaptureForm);
         }
     }
 }
